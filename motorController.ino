f20971eae671c1bd293780259fe2817f2bbb49c3
DaebangStn <gunho1107@naver.com>
2020-12-21 20:08:22 +0900

    fix utp variable issue 2

--- motorController.ino
+++ motorController.ino
@@ -20,7 +20,7 @@ int utc_timestamp;
 int millis_timestamp;
 
 // ntp related variables
-int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 WiFiUDP udp;
 unsigned int localPort = 2390;      // local port to listen for UDP packets
